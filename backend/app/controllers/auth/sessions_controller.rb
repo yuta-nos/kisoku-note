4624019b136add02fa33cbea243bf330738f237e
yuta.n <notfound.no3@live.jp>
2023-09-25 18:37:05 +0900

    sessionによるログイン判定
    sessionsコントローラにてcurrent_userを取得してログイン判定を実装、対応するルーティングを整備

--- backend/app/controllers/auth/sessions_controller.rb
+++ backend/app/controllers/auth/sessions_controller.rb
@@ -1,2 +1,9 @@
 class Auth::SessionsController < ApplicationController
+  def index
+      if current_user
+          render json: {is_login: true, data: current_user, team: current_user.team }
+      else
+          render json: {is_login: false, message: "ユーザーが存在しません"}
+      end
+  end
 end
