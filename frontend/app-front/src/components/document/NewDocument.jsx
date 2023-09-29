02e96bd59aef8c22329e5ffad95e8bb934f4c09e
yuta.n <notfound.no3@live.jp>
2023-09-29 20:22:54 +0900

    新規投稿用のページ実装
    NewDocumentコンポーネント実装、propsでTextEditorにlocationのデータを渡す

---
+++ frontend/app-front/src/components/document/NewDocument.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import TextEditor from './TextEditor'
+
+// route
+import { useLocation } from "react-router-dom";
+
+// styling
+import { Box, Heading, Text } from '@chakra-ui/react'
+
+const NewDocument = () => {
+
+  const location = useLocation();
+
+  return (
+    <Box maxW="750px" my={12} mx="auto" p={3} >
+      <Heading as="h3" size="md">新規作成</Heading>
+      <TextEditor location={location} />
+    </Box>
+  )
+}
+
+export default NewDocument
